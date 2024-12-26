0c6dd4641858fc3fc18f506d8e09e37cd12e202c
AhmadDerieh1 <120324997+AhmadDerieh1@users.noreply.github.com>
2024-12-26 19:43:31 +0200

    Add sentiment analysis to tweet processing

--- Spark/scala/SimulatedTweetIngestion.scala
+++ Spark/scala/SimulatedTweetIngestion.scala
@@ -1,6 +1,8 @@
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import scala.io.Source
 import java.util.Properties
+import scala.util.matching.Regex
+import scala.util.Try
 
 object SimulatedTweetIngestion {
   def main(args: Array[String]): Unit = {
@@ -21,15 +23,42 @@ object SimulatedTweetIngestion {
     val tweets = Source.fromFile(tweetFilePath).getLines()
 
     println(s"Streaming tweets from $tweetFilePath to Kafka topic: $kafkaTopic")
+    val hashtagPattern: Regex = """#\w+""".r
 
+    def analyzeSentimentBasic(text: String): String = {
+      val lowerText = text.toLowerCase()
+      if (lowerText.contains("happy") || lowerText.contains("great") || lowerText.contains("good")) "Positive"
+      else if (lowerText.contains("sad") || lowerText.contains("bad") || lowerText.contains("terrible")) "Negative"
+      else "Neutral"
+    }
 
+    var counter = 0
     try {
       for (tweet <- tweets) {
+        if (counter >= 5) {
+          println("Reached the limit of 5 tweets.")
+          sys.exit(0)
+        }
 
-        val record = new ProducerRecord[String, String](kafkaTopic, tweet)
+
+        val hashtags = hashtagPattern.findAllIn(tweet).mkString(", ")
+
+        val sentiment = analyzeSentimentBasic(tweet)
+
+        val processedTweet =
+          s"""{
+                "original": "$tweet",
+                "hashtags": "$hashtags",
+                 "sentiment": "$sentiment"
+              }"""
+
+        println(s"Processed Tweet: $processedTweet")
+
+        val record = new ProducerRecord[String, String](kafkaTopic, processedTweet)
+
         producer.send(record)
         println(s"Sent: $tweet")
-
+        counter += 1
 
         Thread.sleep(1000)
       }
