b3483ba32e91fe45c213f1387422fb936c5c19ea
AhmadDerieh1 <120324997+AhmadDerieh1@users.noreply.github.com>
2025-01-04 21:34:12 +0200

    Revert "Merge pull request #5 from AhmadDerieh1/dev"
    This reverts commit 7a2c28c98f2e6316bc71af848278a8aa3d7aee4e, reversing
    changes made to 1883060ec5b0e643bae26dc708569953d0a3c144.

--- Spark/scala/MongoDBConnection.scala
+++
@@ -1,9 +0,0 @@
-import org.mongodb.scala._
-
-object MongoDBConnection {
-
-  val mongoClient: MongoClient = MongoClient("mongodb://localhost:27017")
-  val database: MongoDatabase = mongoClient.getDatabase("tweets_db")
-
-  def getDatabase: MongoDatabase = database
-}
